2e9efbf75b59008ca6b16872c73bd81eec041746
Jeris Tanus <jeris.tanus@gmail.com>
2018-03-09 19:06:29 +0200

    Removed the guestbook app.

--- guestbook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Guestbook
-
-class GuestbookAdmin(admin.ModelAdmin):
-    list_display = ['entry_text', 'writer_nickname', 'entry_datetime']
-
-admin.site.register(Guestbook, GuestbookAdmin)
